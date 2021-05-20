23f89c0abdfd3dfb5964f2a523f19f398ec36541
Grisha <grigorije05@gmail.com>
2021-05-20 15:05:06 +0200

    animirao + dugme

---
+++ assets/js/animation.js
@@ -0,0 +1,9 @@
+function animatePlus() {
+    $("#x").css({"transform": "scale(15) translateY(1px)"})
+    $("#xpurple").css({"opacity": "1"})
+}
+
+function deAnimatePlus() {
+    $("#x").css({"transform": ""})
+    $("#xpurple").css({"opacity": "0"})
+}
